a5c93e15c2c19b6fa4b58fc6a2ffb817ee7bdb0d
Ahmed Hany <90726408+AHMEDHANY18@users.noreply.github.com>
2024-08-15 00:18:08 +0300

    Update user.model.js

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -3,6 +3,7 @@ const { object } = pkg;
 import { Schema, model } from 'mongoose';
 import { systemRole } from '../../Utility/sysrole.js';
 
+
 const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -16,8 +17,16 @@ const userSchema = new Schema({
         type: String,
         enum: Object.values(systemRole),
         default: systemRole.user
-    }
+    },
+    addresses: [{
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        zip: { type: String, required: true },
+        country: { type: String, required: true }
+    }]
 }, { timestamps: true });
 
 const User = model('User', userSchema);
 export default User;
+
