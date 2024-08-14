d77869dfc3b8f548cf476c4d59258e79e0fce294
Ahmed Hany <90726408+AHMEDHANY18@users.noreply.github.com>
2024-08-14 21:33:26 +0300

    Update wishList.controller.js

--- src/modules/wishList/wishList.controller.js
+++ src/modules/wishList/wishList.controller.js
@@ -34,5 +34,29 @@ export const createWishList = asyncHandler(async (req, res, next) => {
 
 })
 
+// deleteWishList
+export const deleteWishList = asyncHandler(async (req, res, next) => {
+    const { productId } = req.params;
+    const product = await productModel.findById({ _id: productId });
+    if (!product) {
+        return next(new AppError("Product not found", 404));
+    }
+    const wishList = await wishListModel.findOne({ user: req.user._id });
+    if (!wishList) {
+        return next(new AppError("Wishlist not found", 404));
+    }
+    if (!wishList.products.includes(productId)) {
+        return next(new AppError("Product not found in wishlist", 404));
+    }
+    wishList.products = wishList.products.filter(
+        (item) => item.toString() !== productId.toString()
+    );
+    if (wishList.products.length === 0) {
+        await wishListModel.findOneAndDelete({ user: req.user._id });
+        return res.status(200).json({ msg: "Wishlist deleted " });
+    }
+    await wishList.save();
+    res.status(200).json({ msg: "Product removed from wishlist", wishList });
+});
 
 
