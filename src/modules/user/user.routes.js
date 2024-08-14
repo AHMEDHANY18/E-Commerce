7dc87428742412a6ad27d5c2f1f59b3172e41f2f
Ahmed Hany <90726408+AHMEDHANY18@users.noreply.github.com>
2024-08-15 00:17:39 +0300

    Update user.routes.js

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -6,11 +6,21 @@ import { validate } from "../../middelware/validation.js";
 import * as UV from "./userValidation.js";
 import { systemRole } from "../../../Utility/sysrole.js";
 
+// User authentication routes
 router.post("/signup", validate(UV.signupValidation), UC.signup);
 router.post("/signin", validate(UV.signinValidation), UC.signin);
-router.get("/", UC.getAllUsers);
+
+// User management routes
+router.get("/", auth(Object.values(systemRole)), UC.getAllUsers);
+router.post("/confirm", UC.confirmEmail);
+
+// Password management routes
 router.post("/requestPasswordReset", validate(UV.requestPasswordResetValidation), UC.requestPasswordReset);
 router.patch('/updatePassword', auth(Object.values(systemRole)), validate(UV.updatePasswordValidation), UC.updatePassword);
 router.post("/resetPassword", validate(UV.resetPasswordValidation), UC.resetPassword);
-router.post("/confirm", UC.confirmEmail);
+
+// Address management routes
+router.patch('/updateAddress', auth(Object.values(systemRole)), validate(UV.updateAddressValidation), UC.updateAddress);
+router.delete('/deleteAddress', auth(Object.values(systemRole)), validate(UV.deleteAddressValidation), UC.deleteAddress);
+
 export default router;
